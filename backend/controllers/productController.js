e70f1a158c2e189c3c186f232a0513b0fc8087ae
Khenrab <tenkhen@gmail.com>
2023-12-30 00:01:13 -0500

    Carousel bug fix

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -143,7 +143,7 @@ const createProductReview = asyncHandler(async (req, res) => {
 // @route   GET /api/products/top
 // @access  public
 const getTopProducts = asyncHandler(async (req, res) => {
-  const products = await Product.findById({}).sort({ rating: -1 }).limit(3);
+  const products = await Product.find({}).sort({ rating: -1 }).limit(3);
   res.status(200).json(products);
 });
 
